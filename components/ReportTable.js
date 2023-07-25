a855b970d967a110d5518d973a6646b16746a3ec
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-07-25 21:31:21 +0300

    done

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -1,11 +1,16 @@
 import data, { hours, totals } from "@/data";
-
-export default function ReportTable({ formdata }) {
+import { FaTrashAlt } from "react-icons/fa";
+export default function ReportTable({ formdata, setFormdata }) {
+  function handleDelete(indx) {
+    let newdata = formdata.filter((i, index) => index !== indx);
+    setFormdata(newdata);
+    console.log(newdata);
+  }
   return (
     <table className="pt-20">
       <thead>
         <tr className="bg-green-600">
-          <th className="px-5">Location</th>
+          <th className="px-5 w-52">Location</th>
           {hours.map((hour) => (
             <th className="px-5">{hour}</th>
           ))}
@@ -15,8 +20,11 @@ export default function ReportTable({ formdata }) {
       <tbody>
         {formdata.map((item, indx) => (
           <tr className={indx % 2 === 0 ? "bg-green-400" : "bg-green-500"}>
-            <td className="border border-black py-2 text-center">
-              {item.location}
+            <td className="flex justify-between p-5 border border-black py-2 text-center">
+              <p>{item.location}</p>
+              <button onClick={() => handleDelete(indx)}>
+                <FaTrashAlt />
+              </button>
             </td>
             {data.map((i) => (
               <td className="border border-black py-2 text-center">{i}</td>
@@ -27,8 +35,11 @@ export default function ReportTable({ formdata }) {
       <tfoot>
         <tr className="bg-green-600">
           <td className="border border-black text-center font-bold">Totals</td>
-          {data.map((y) => (
-            <td className="border border-black text-center font-bold" key={y}>
+          {data.map((y, indx) => (
+            <td
+              className="border border-black text-center font-bold"
+              key={indx}
+            >
               {y * formdata.length}
             </td>
           ))}
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,29 @@ import Head from "next/head";
 import Main from "@/components/Main";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuth } from "@/context/authcontext";
+import { LoginForm } from "@/components/LoginForm";
 
 export default function Home() {
   const [formdata, setFormdata] = useState([]);
+  const { user } = useAuth();
+  // let user = null;
+
   return (
     <>
       <Head>
         <title>Cookie Stand Admin</title>
       </Head>
-      <div className="flex flex-1 min-h-screen flex-col">
-        <Header />
-        <Main formdata={formdata} setFormdata={setFormdata} />
-        <Footer formdata={formdata} />
-      </div>
+      {user ? (
+        <div className="flex flex-1 min-h-screen flex-col">
+          <Header />
+          <Main formdata={formdata} setFormdata={setFormdata} />
+          <Footer formdata={formdata} />
+        </div>
+      ) : (
+        <LoginForm />
+      )}
     </>
   );
 }
